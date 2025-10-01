ee007c90eb9da8a02ccfc0e7ae28dcbe306dbbd1
aldiworkspace <aldirizkibagus905@gmail.com>
2025-10-01 15:14:56 +0700

    Add files via upload

---
+++ UNION.sql
@@ -0,0 +1,48 @@
+#UNION
+#UNION dalam SQL digunakan untuk menggabungkan hasil dari dua atau lebih kueri SELECT menjadi satu tumpukan hasil (result set) tunggal
+#UNION menggabungkan data ke bawah (menumpuk baris dari kueri lain).
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION ALL
+SELECT first_name, last_name
+FROM employee_salary
+;
+
+#UNION ALL akan menyertakan semua baris dari setiap kueri SELECT, termasuk jika ada baris yang duplikat. 
+#Karena tidak perlu memeriksa duplikat, UNION ALL lebih cepat dan lebih efisien daripada UNION
+
+SELECT first_name, last_name
+FROM employee_demographics
+UNION 
+SELECT first_name, last_name
+FROM employee_salary
+;
+
+#UNION
+#Secara default, UNION akan menghapus baris duplikat dari hasil gabungan. 
+#Ia hanya akan menampilkan nilai yang unik.
+
+SELECT first_name, last_name, 'OLD' AS Label
+FROM employee_demographics
+WHERE age > 50
+UNION
+SELECT first_name, last_name, 'Highest paid employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+;
+
+SELECT first_name, last_name, 'OLD man' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Male'
+UNION
+SELECT first_name, last_name, 'OLD female' AS Label
+FROM employee_demographics
+WHERE age > 40 AND gender = 'Female'
+UNION
+SELECT first_name, last_name, 'Highest paid employee' AS Label
+FROM employee_salary
+WHERE salary > 70000
+ORDER BY first_name, last_name
+;
+
